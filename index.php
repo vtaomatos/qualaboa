5667745bdb68e5d4542e5362732ece3368dae80a
Vitor Matos <vamatos48@hotmail.com>
2025-07-16 01:38:48 -0300

    Melhorias de layout e usabilidade do site. Adição de controle de visibilidade do topo e rodape, e slide para mais de um evento no mesmo local mesmo dia. Ajuste no tamanho do mapa e elementos.

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ try {
 date_default_timezone_set('America/Sao_Paulo');
 $dataFiltro = $_GET['data'] ?? date('Y-m-d');
 $horaFiltro = $_GET['hora'] ?? '00:00';
+$dataFormatada = htmlspecialchars($_GET['data'] ?? date('d/m/Y'));
 
 // Preparar e executar query
 $sql = "SELECT * FROM eventos WHERE DATE(data_evento) = :data AND TIME(data_evento) >= :hora";
@@ -39,23 +40,27 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <!-- Fonte e CSS personalizado -->
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="./css/main.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
 </head>
 
 
 <body class="text-center">
-  <div id="regiao-1">
-    <h1 class="text-primary fw-bold mt-3">Encontre Eventos</h1>
+  <div id="regiao-1" class="fixo-topo">
     <!-- <p class="text-center mb-3">Qual a boa do dia?!</p> -->
-  </div>
-  <div id="regiao-2">
-    <!-- Filtro de data e hora -->
-    <form method="GET" class="container-fluid pb-1 pt-1">
+    <form id="form-regiao-1" method="GET" class="container-fluid pb-1 pt-1 regiao-colapsada">
       <label class="col-7">Data: <input class="form-control" type="date" name="data"
           value="<?= htmlspecialchars($dataFiltro) ?>" /></label>
       <label class="col-5">Hora: <input class="form-control" type="time" name="hora"
           value="<?= htmlspecialchars($horaFiltro) ?>" /></label>
       <button type="submit" class="col-12 mt-2 btn btn-primary buscar-data">Buscar</button>
     </form>
+    <button class="toggle-btn" onclick="toggleRegiao('form-regiao-1',this)">⮟ Encontre eventos na sua região por data:
+      <?= $dataFormatada ?></button>
+  </div>
+
+  <div id="regiao-2">
+    <!-- Filtro de data e hora -->
 
     <!-- Mapa -->
     <div id="map" class="mt-2 mb-2"></div>
@@ -70,14 +75,14 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <?php endforeach; ?> -->
   </div>
 
-  <div id="regiao-3" class="container-fluid">
+  <div id="regiao-3" class="fixo-rodape">
     <!-- Chat com IA -->
     <div class="col-12 pd-2 chat-area">
-      <h2 class="mb-0">O que está procurando?</h2>
-      <small class="text-muted">"Sou a IA qual é a boa. Vou te ajudar achar o rolê!"</small>
-      <div id="chatInput" class="chat-input">
+      <!-- <h2 class="mb-0">Encontre Eventos Com IA</h2> -->
+      <!-- <small class="text-muted">"Sou a IA qual é a boa. Vou te ajudar achar o rolê!"</small> -->
+      <div id="chatInput" class="chat-input regiao-colapsada">
         <input class="form-control mt-3" type="text" id="userInput"
-          placeholder="Ex: Me indique algo no Gonzaga hoje à noite" />
+          placeholder="Ex: IA, me indique um sambinha perto do centro à noite" />
         <button class="btn btn-secondary btn-chat col-12 mt-2" onclick="enviarPergunta()">Enviar</button>
       </div>
       <div id="recomendacoesChat" class="recomendacoes-container">
@@ -86,8 +91,14 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <p id="explicacaoIA" class="explicacao-chat"></p>
 
     </div>
+    <button class="toggle-btn toggle-btn-chatInput" onclick="toggleRegiao('chatInput', this)">⮝ Use Ia para encontrar eventos para dia <?= $dataFormatada ?></button>
   </div>
+  <div id="lightbox" onclick="fecharLightbox()">
+    <img id="lightbox-img" src="" alt="Imagem Ampliada" />
+  </div>
+
   <script>
+    const dataFiltro = "<?= $dataFormatada ?>";
     const eventos = <?= json_encode($eventos) ?>;
     let ordemPrioridade = [];
 
@@ -97,10 +108,12 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     function criarMapa(center) {
       return new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        scrollwheel: false,
+        zoom: 12,
         gestureHandling: "greedy",
         center,
+        mapTypeControll:false,
+        streetViewControl: false,
+        zoomControl: true,
       });
     }
 
@@ -116,33 +129,6 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
       });
     }
 
-    function criarInfoWindow(evento) {
-      const instagramTag = evento.linkInstagram
-        ? `<a href="${evento.linkInstagram}" target="_blank">${evento.instagram}</a><br>`
-        : evento.instagram
-          ? `<span>${evento.instagram}</span><br>`
-          : '';
-
-      let flyerContent = '';
-
-      if (evento.imagem_base64) {
-        flyerContent = `<img src="data:image/png;base64,${evento.imagem_base64}" alt="Flyer" style="max-width: 80%;">`;
-      } else if (evento.flyer_imagem) {
-        flyerContent = `<img src="${evento.flyer_imagem}" alt="Flyer" style="max-width: 80%;">`;
-      } else if (evento.flyer_html) {
-        flyerContent = evento.flyer_html;
-      }
-
-      return `
-        <div id="${evento.id}" style="max-width: 250px; font-family: Arial, sans-serif;">
-          <h3 style="margin: 0 0 8px 0; font-size: 16px; color: #333;">${evento.titulo}</h3>
-          <div style="max-height: 200px; overflow-y: auto; border-top: 1px solid #ccc; padding-top: 8px; font-size: 14px; color: #555;">
-            ${flyerContent}
-          </div>
-          ${instagramTag}
-        </div>
-      `;
-    }
 
 
     function getTamanhoPorPrioridade(id) {
@@ -165,63 +151,15 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
       if (!pos) return;
 
       const marker = criarMarker(map, pos, evento.titulo, "https://maps.google.com/mapfiles/ms/icons/red-dot.png", tamanho);
-      const infoWindow = new google.maps.InfoWindow({ content: infoContent });
+      const infoWindow = new google.maps.InfoWindow({ content: infoContent, disableAutoPan: false });
 
-      marker.addListener("click", () => infoWindow.open(map, marker));
-    }
-
-    function mostrarLocalizacaoComAnimacao(map) {
-      if (!navigator.geolocation) {
-        console.warn("Geolocalização não suportada.");
-        return;
-      }
-
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          const userLoc = {
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude,
-          };
-
-          // // 🔵 Cria marcador azul
-          // const marcador = criarMarker(
-          //   map,
-          //   userLoc,
-          //   "Você está aqui",
-          //   "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-          //   40
-          // );
-
-          // 🟦 Cria círculo de precisão
-          const circulo = new google.maps.Circle({
-            strokeColor: "#1E90FF",
-            strokeOpacity: 0.6,
-            strokeWeight: 2,
-            fillColor: "#1E90FF",
-            fillOpacity: 0.2,
-            map: map,
-            center: userLoc,
-            radius: pos.coords.accuracy || 50,
-          });
-
-          map.setCenter(userLoc);
-
-          // 🔄 Anima o raio do círculo
-          let growing = true;
-          let currentRadius = circulo.getRadius();
-
-          setInterval(() => {
-            currentRadius += growing ? 5 : -5;
-            circulo.setRadius(currentRadius);
-
-            if (currentRadius >= (pos.coords.accuracy + 30)) growing = false;
-            if (currentRadius <= pos.coords.accuracy) growing = true;
-          }, 80);
-        },
-        (err) => {
-          console.warn("Erro ao obter localização:", err.message);
-        }
-      );
+      marker.addListener("click", () => {
+        infoWindow.open(map, marker);
+        map.panBy(0, -250);
+        google.maps.event.addListenerOnce(infoWindow, 'closeclick', () => {
+          map.panBy(0, 250); // Reverte o deslocamento
+        });
+      });
     }
 
     function initMap() {
@@ -230,8 +168,40 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
       mostrarLocalizacaoComAnimacao(map);
 
-      eventos.forEach((evento) => {
-        adicionarEventoNoMapa(map, evento);
+      const eventosAgrupados = agruparEventosPorLocal(eventos);
+
+      Object.keys(eventosAgrupados).forEach((key) => {
+        const eventosDoLocal = eventosAgrupados[key];
+        const exemploEvento = eventosDoLocal[0];
+
+        const pos = {
+          lat: parseFloat(exemploEvento.latitude),
+          lng: parseFloat(exemploEvento.longitude)
+        };
+
+        const tamanho = getTamanhoPorPrioridade(exemploEvento.id);
+        const marker = criarMarker(map, pos, exemploEvento.titulo, "https://maps.google.com/mapfiles/ms/icons/red-dot.png", tamanho);
+        const infoWindow = new google.maps.InfoWindow({ disableAutoPan: false });
+
+        marker.addListener("click", () => {
+          const infoWindowContent = criarSliderEventos(eventosDoLocal);
+          infoWindow.setContent(infoWindowContent);
+          infoWindow.open(map, marker);
+          map.panBy(0, -250);
+
+          google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+            new Swiper('.swiper-container', {
+              pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+              },
+            });
+          });
+
+          google.maps.event.addListenerOnce(infoWindow, 'closeclick', () => {
+            map.panBy(0, 250);
+          });
+        });
       });
     }
 
@@ -272,9 +242,142 @@ $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
           document.scrollingElement.scrollTo(0, 999999);
         });
     }
+
+    function toggleRegiao(regiaoId, btn) {
+      const regiao = document.getElementById(regiaoId);
+      const colapsada = regiao.classList.toggle('regiao-colapsada');
+
+      const mostrarTexto = regiaoId === 'chatInput' ? `⮝ Use Ia para encontrar eventos para dia ${dataFiltro}` : `⮟ Encontre eventos na sua região por data: ${dataFiltro}`;
+      const ocultarTexto = regiaoId === 'chatInput' ? '⮟ Ocultar' : '⮝ Ocultar';
+
+      btn.innerHTML = colapsada ? mostrarTexto : ocultarTexto;
+    }
+
+    function agruparEventosPorLocal(eventos) {
+      const agrupados = {};
+      eventos.forEach((evento) => {
+        const key = `${parseFloat(evento.latitude).toFixed(5)}_${parseFloat(evento.longitude).toFixed(5)}`;
+        if (!agrupados[key]) agrupados[key] = [];
+        agrupados[key].push(evento);
+      });
+      return agrupados;
+    }
+
+function criarSliderEventos(eventos) {
+  if (!eventos.length) return '';
+
+  let slides = eventos.map((ev) => {
+    const imagem = ev.imagem_base64
+      ? `<img src="data:image/png;base64,${ev.imagem_base64}" class="slide-img" onclick="abrirLightbox(this)" style="max-width:70%">`
+      : ev.flyer_imagem
+        ? `<img src="${ev.flyer_imagem}" class="slide-img" onclick="abrirLightbox(this)" />`
+        : ev.flyer_html || '';
+
+    const instagram = ev.linkInstagram
+      ? `<a href="${ev.linkInstagram}" target="_blank">${ev.instagram}</a><br>`
+      : ev.instagram
+        ? `<span>${ev.instagram}</span><br>`
+        : '';
+
+    return `
+      <div class="swiper-slide" style="text-align:center">
+        <h3 style="font-size:16px; margin-bottom:5px; white-space: normal; word-break: break-word; margin-right:15px">
+          ${ev.titulo}
+        </h3>
+        ${imagem}
+        <div style="margin-top:6px;">${instagram}</div>
+      </div>
+    `;
+  }).join('');
+
+  return `
+    <div class="swiper-container" style="width: 230px; max-width: 230px;">
+      <div class="swiper-wrapper">
+        ${slides}
+      </div>
+      <div class="swiper-pagination" style="margin-top: 5px;"></div>
+    </div>
+  `;
+}
+
+    function mostrarLocalizacaoComAnimacao(map) {
+      if (!navigator.geolocation) return;
+
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const userLoc = {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          };
+
+          const circulo = new google.maps.Circle({
+            strokeColor: "#1E90FF",
+            strokeOpacity: 0.6,
+            strokeWeight: 2,
+            fillColor: "#1E90FF",
+            fillOpacity: 0.2,
+            map: map,
+            center: userLoc,
+            radius: pos.coords.accuracy || 50,
+          });
+
+          map.setCenter(userLoc);
+
+          let growing = true;
+          let currentRadius = circulo.getRadius();
+
+          setInterval(() => {
+            currentRadius += growing ? 5 : -5;
+            circulo.setRadius(currentRadius);
+
+            if (currentRadius >= (pos.coords.accuracy + 30)) growing = false;
+            if (currentRadius <= pos.coords.accuracy) growing = true;
+          }, 80);
+        },
+        (err) => {
+          console.warn("Erro ao obter localização:", err.message);
+        }
+      );
+    }
+
+
+
+    marker.addListener("click", () => {
+      const key = `${parseFloat(evento.latitude).toFixed(5)}_${parseFloat(evento.longitude).toFixed(5)}`;
+      const eventosDoLocal = eventosAgrupados[key] || [];
+
+      const infoWindowContent = criarSliderEventos(eventosDoLocal);
+      infoWindow.setContent(infoWindowContent);
+      infoWindow.open(map, marker);
+      map.panBy(0, -250);
+
+      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+        new Swiper('.swiper-container', {
+          pagination: { el: '.swiper-pagination', clickable: true },
+        });
+      });
+
+      google.maps.event.addListenerOnce(infoWindow, 'closeclick', () => {
+        map.panBy(0, 250);
+      });
+    });
+
+
+    function abrirLightbox(imgElement) {
+      document.getElementById('lightbox-img').src = imgElement.src;
+      document.getElementById('lightbox').style.display = 'flex';
+    }
+
+    function fecharLightbox() {
+      document.getElementById('lightbox').style.display = 'none';
+    }
+
   </script>
+
   <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfCUegcuOqp8VUDdwJeYt9EoIGh4T0zPs&callback=initMap"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
 </body>
 
 </html>
